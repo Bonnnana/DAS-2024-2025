9ed8fac16975b7331e6782c68d3b8f06d18bca09
Bonnnana <bojana.andonova03@gmail.com>
2024-11-10 11:48:28 +0100

    Uploading firts homework

---
+++ filters/base_filter.py
@@ -0,0 +1,9 @@
+"""FILTER BASE """
+
+from abc import ABC, abstractmethod
+
+
+class Filter(ABC):
+    @abstractmethod
+    def process(self, data):
+        pass
---
+++ filters/import_all_stocks_filter.py
@@ -0,0 +1,35 @@
+"""FILTER 1"""
+
+import requests
+from bs4 import BeautifulSoup
+from typing import List
+
+from filters.base_filter import Filter
+
+
+class ImportAllStocksFilter(Filter):
+    def __init__(self):
+        self.url = 'https://www.mse.mk/en/stats/symbolhistory/ADIN'
+
+    def get_valid_issuers(self) -> List[str]:
+        response = requests.get(self.url)
+        response.raise_for_status()
+
+        raw_html = response.text
+        soup = BeautifulSoup(raw_html, 'html.parser')
+
+        issuers = soup.find_all('option')
+
+        imported_issuers = []
+        for issuer in issuers:
+            issuer_name = issuer.text.strip()
+            if issuer_name and issuer_name.isalpha():
+                imported_issuers.append(issuer_name)
+
+        return imported_issuers
+
+    def process(self, data) -> List[str]:
+        valid_issuers = self.get_valid_issuers()
+        return valid_issuers
+
+
